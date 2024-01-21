7a78016de473fd4a12951dd6efb212aa7e1cf1fc
presadicorrente <116338639+presadicorrente@users.noreply.github.com>
2024-01-21 11:18:28 +0100

    bottone che cambia non piscia pesci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
 'use client'
+import React, { useState } from 'react';
 
-async function res(){
+
+
+export default function Home() {
+  const [isOn, setIsOn] = useState(false);
+
+  async function res(){
+    //fetch della get per capire lo stato del led
     const res = await fetch("https://esp32-jj7p.onrender.com/getAll", {method : 'GET'})
     let data = await res.json();
     console.log(JSON.stringify(data))
+    //cambio della variabile dello stato del led 
     let primoled = data[0]
-    
     if(primoled.status) primoled.status = false
     else primoled.status = true
     console.log("primo led "+ JSON.stringify(primoled))
+    //fetch della post per cambiare lo stato del led nel db
     const resPut = await fetch("https://esp32-jj7p.onrender.com/modifyExe", 
       {method: 'POST', 
       headers: {'Content-Type': 'application/json;charset=utf-8'},
        body: JSON.stringify(primoled)
       })
+    //spoiler funziona
+    setIsOn(primoled.status);
     console.log(resPut)
-}
+  }
 
-export default function Home() {
     return (
       <div className="h-screen bg-black flex flex-col items-center justify-center"  >
         <h1 className='text-white text-center text-4xl m-5'>
@@ -26,7 +35,7 @@ export default function Home() {
         <h1 className='text-white text-center text-4xl m-5'>
           riesco a comandare il led da tutte le parti del mondo
         </h1>
-        <button className="p-3 w-1/2 m-3 text-4xl text-center rounded-md bg-white hover:bg-gray-100" onClick={res}>On/Off</button>
+        <button className="p-3 w-1/2 m-3 text-4xl text-center rounded-md bg-white hover:bg-gray-100" onClick={res}>{isOn ? 'Spegni' : 'Accendi'}</button>
       </div>
     )
 }
